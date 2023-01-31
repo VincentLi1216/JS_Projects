af02a5457f29d379805ac56363fcd4dc333f2430
VincentLi1216 <sunnus.tw@gmail.com>
2023-01-31 20:16:16 +0800

    add Multiplication_App

---
+++ Multiplication_App/index.js
@@ -0,0 +1,50 @@
+const num1 = ramdom(1, 9);
+const num2 = ramdom(1, 9);
+const correctAns = num1 * num2;
+let score;
+const savedScore = JSON.parse(localStorage.getItem("score"))
+
+if (savedScore){
+  score = 0;
+}else{
+  score = savedScore;
+}
+
+const questionEl = document.getElementById("question");
+const inputEl = document.getElementById("input");
+const btnEl = document.getElementById("btn");
+const scoreEl = document.getElementById("score");
+const formEl = document.getElementById("form");
+
+function render(){
+  scoreEl.innerText = `Score: ${score}`;
+  
+
+}
+questionEl.innerText = `What is ${num1} x ${num2}?`;
+
+function ramdom(min, max) {
+  ram_num = Math.random() * (max - min) + min;
+  return Math.round(ram_num);
+}
+
+formEl.addEventListener("submit", () => {
+  const userAns = +inputEl.value;
+  console.log(userAns)
+  if (userAns === correctAns) {
+    score++;
+    saveScore();
+    render();
+    console.log(score);
+  } else {
+    score--;
+    saveScore();
+    render();
+    console.log(score);
+  }
+});
+
+function saveScore() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
+render();
